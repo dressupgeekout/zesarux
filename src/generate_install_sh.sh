e6a7217b11e63e056f071b44dfdf522701ff6fcc
Charlotte Koch <dressupgeekout@gmail.com>
2020-08-09 15:25:11 -0700

    Also require DESTDIR support

--- src/generate_install_sh.sh
+++ src/generate_install_sh.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
-INSTALLPREFIX=`cat compileoptions.h |grep INSTALL_PREFIX|cut -d '"' -f2`
+prefix=`cat compileoptions.h |grep INSTALL_PREFIX|cut -d '"' -f2`
+INSTALLPREFIX=${DESTDIR}/${prefix}
 
 
 cat > install.sh << _EOF
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Installing ZEsarUX under $INSTALLPREFIX ..."
 
